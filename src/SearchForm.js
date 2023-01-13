389de202df1d1d823bf530c5c0317bbe72751a20
mako542b <marcin.piotr.konopka@gmail.com>
2023-01-13 20:14:51 +0100

    m

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,6 +1,6 @@
 import SortElement from './SortElement'
 import { ThemeContext } from './Theme'
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 
 
 const SearchForm = ({
@@ -9,40 +9,48 @@ const SearchForm = ({
 }) => {
 
     const {theme} = useContext(ThemeContext)
+    const [display, setDisplay] = useState(true)
 
-    return(
-        <div className='search-form'>
-            <form className="region-form" onSubmit={(e)=>e.preventDefault()}>
-                <input 
-                    type="text" 
-                    style={{backgroundColor: theme.backgroundElements, color:theme.color}} 
-                    placeholder="search for a country..." 
-                    className="region-input | bkg-elements box-shadow" 
-                    onChange={(e) => setSearchOptions(prev => ({...prev, filterCountry: e.target.value}))} 
-                    value={searchOptions.filterCountry}
-                >
-                </input>
-                <SortElement setSearchOptions={setSearchOptions} searchOptions={searchOptions}/>
-                <select 
-                    className="option-input | bkg-elements f-w-300 box-shadow" 
-                    style={{backgroundColor: theme.backgroundElements, color:theme.color}} 
-                    name="region" 
-                    id="region" 
-                    // defaultValue={searchOptions.filterRegion} 
-                    onChange={(e) => setSearchOptions(prev =>  ({...prev, filterRegion: e.target.value}))}
-                >
-                    <option value="default" disabled hidden>
-                        filter by region    
-                    </option>
-                    <option value="all">All</option>
-                    <option value="africa">Africa</option>
-                    <option value="europe">Europe</option>
-                    <option value="americas">America</option>
-                    <option value="oceania">Oceania</option>
-                    <option value="asia">Asia</option>
-                </select>
-            </form>
-        </div>
+    return (
+        <>
+            {display ? (<div 
+                className='search-form' 
+                style={{backgroundColor:theme.backgroundBody}}
+            >
+                <form className="region-form" onSubmit={(e)=>e.preventDefault()}>
+                    <input 
+                        type="text" 
+                        style={{backgroundColor: theme.backgroundElements, color:theme.color}} 
+                        placeholder="search for a country..." 
+                        className="region-input | bkg-elements box-shadow" 
+                        onChange={(e) => setSearchOptions(prev => ({...prev, filterCountry: e.target.value}))} 
+                        value={searchOptions.filterCountry}
+                    >
+                    </input>
+                    <SortElement setSearchOptions={setSearchOptions} searchOptions={searchOptions}/>
+                    <select 
+                        className="option-input | bkg-elements f-w-300 box-shadow" 
+                        style={{backgroundColor: theme.backgroundElements, color:theme.color}} 
+                        name="region" 
+                        id="region" 
+                        onChange={(e) => setSearchOptions(prev =>  ({...prev, filterRegion: e.target.value}))}
+                    >
+                        <option value="default" disabled hidden>
+                            filter by region    
+                        </option>
+                        <option value="all">All</option>
+                        <option value="africa">Africa</option>
+                        <option value="europe">Europe</option>
+                        <option value="americas">America</option>
+                        <option value="oceania">Oceania</option>
+                        <option value="asia">Asia</option>
+                    </select>
+                    <button className='close-options-button' type='button' onClick={() => setDisplay(false)}>X</button>
+                </form>
+            </div> ) : (
+                <button className='display-button' onClick={() => setDisplay(true)}>Search options</button>
+            )}
+        </>
     )
 }
 
